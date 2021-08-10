20ce86234a5a00694870031057182758a617b0a8
Elena Leusik <elena.leusik@gmail.com>
2021-08-10 18:05:47 +0300

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  if (!array || !array.length) { return 0; }
+    
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < result) {
+      result = array[i];
+    }      
+  }
+  return result;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) { return 0 ; }
+  
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > result) {
+      result = array[i];
+    }
+  }
+  return result;
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) { return 0; }
+  
+  let sum = 0;
+  let result;
+  for (let i = 0 ; i < array.length; i++) {
+    sum += array[i];
+  }
+  result = sum / array.length;
+  return result;   
+  
 }
+
+
